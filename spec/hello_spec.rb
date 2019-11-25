da78147e41545f8ee5d64ec76b00cb931b89da28
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2019-11-25 16:48:35 +0900

    Test PR

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Hello, "#greetings" do
       hello = Hello.new
       expect(hello.greetings).to eq 'Hello, Ruby'
     end
+
+    it "can tell you your secrets?" do
+      puts "#{ENV['SCM_USERNAME']}"
+    end
   end
 end
